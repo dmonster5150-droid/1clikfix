312fc263235b4ef31608ee615b6e7f49f29c089c
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-04 15:22:28 -0800

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p>© {new Date().getFullYear()} 1ClikFix. All rights reserved.</p>
+    </footer>
+  );
+}
