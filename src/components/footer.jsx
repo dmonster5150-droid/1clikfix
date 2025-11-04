72f85fb25bd3b7a1d8ddc1950a4f707118676567
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-04 15:24:16 -0800

    Delete src/components directory

--- src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      <p>© {new Date().getFullYear()} 1ClikFix. All rights reserved.</p>
-    </footer>
-  );
-}
