c2c2ef4181248165b11e47dad7540291f328a642
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-04 12:05:08 -0800

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html","./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0066ff',
+        redbrand: '#d32f2f',
+        blackish: '#0a0a0a'
+      },
+      fontFamily: {
+        inter: ['Inter','ui-sans-serif','system-ui']
+      }
+    }
+  },
+  plugins: []
+}
