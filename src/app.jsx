9be7b641cb3159186e4f0450625e9614d820fbeb
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-03 22:43:01 -0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import Client from "./pages/Client.jsx";
+import Provider from "./pages/Provider.jsx";
+import About from "./pages/About.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow max-w-5xl mx-auto px-4 py-8 w-full">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <nav className="p-4 bg-red-600 text-white flex justify-between">
+        <h1 className="font-bold text-lg">1ClikFix</h1>
+        <div className="space-x-4">
+          <Link to="/">Home</Link>
+          <Link to="/client">Client</Link>
+          <Link to="/provider">Provider</Link>
+          <Link to="/about">About</Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/client" element={<Client />} />
+        <Route path="/provider" element={<Provider />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
