3b2f17ad6fd3f319bd2e6dd05f4621f2719759fa
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-06 08:57:15 -0800

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,4 @@
-import { Link } from "react-router-dom";
-
-<Link to="/client">Client</Link>
-<Link to="/provider">Provider</Link>
-<Link to="/booking">Booking</Link>
-<Link to="/calendar">Calendar</Link>
-<Link to="/subscribe">Subscribe</Link>
-<Link to="/about">About</Link> React from "react";
-import { Link } from "react-router-dom";
-
-export default function Navbar() {
-  return (
-    <nav className="bg-white shadow p-4 flex justify-between">
-      <Link to="/" className="font-bold text-xl text-red-600">1ClikFix</Link>
-      <div className="flex gap-4">
-        <Link to="/about">About</Link>
-        <Link to="/agreements">Agreements</Link>
-      </div>
-    </nav>
-  );
-}<a
+<a
   href="https://1clikfixsquare.square.site/"
   target="_blank"
   rel="noopener noreferrer"
