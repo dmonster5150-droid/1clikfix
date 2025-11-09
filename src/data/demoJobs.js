8b3e63439eb34ebc17b63365b5e3bd88847c979a
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-09 04:23:54 -0800

    Delete src directory

--- src/data/demoJobs.js
+++
@@ -1,7 +0,0 @@
-export const DEMO_JOBS = [
-  { id:'j1', title:'Basic Handyman - Small repairs', desc:'Small repairs around the home', price:4999 },
-  { id:'j2', title:'Plumbing - Minor', desc:'Leaks, faucet replacements', price:6999 },
-  { id:'j3', title:'Electrical - Minor', desc:'Switches, outlets', price:7999 },
-  { id:'j4', title:'Yard Work', desc:'Mow, trim, debris removal', price:3999 },
-  { id:'j5', title:'Painting - Patch & touch', desc:'Small painting jobs', price:5999 }
-];
