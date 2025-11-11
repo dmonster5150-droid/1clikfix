8821107635d21945ea5bbfc5503f213c5f76807d
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-11 15:13:06 -0800

    Update create.jobs.js

--- netlify/functions/create.jobs.js
+++ netlify/functions/create.jobs.js
@@ -1,1 +1,11 @@
-
+exports.handler = async (event, context) => {
+  try {
+    const job = JSON.parse(event.body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, message: "Job created!", job }),
+    };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ success: false, error: err.message }) };
+  }
+};
