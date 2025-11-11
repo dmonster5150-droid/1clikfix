02d0836c4cb184006a08ec12755146044997f02f
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-11 12:28:35 -0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,13 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#c40000",
+        dark: "#111111",
+        grayish: "#2b2b2b"
+      },
+    },
+  },
   plugins: [],
-};
+}
