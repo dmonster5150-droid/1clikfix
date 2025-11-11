127d80a87dddcfd40f73fd1a849e7576eb222b8c
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-11 13:52:37 -0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,20 @@
-
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Calendar from "./pages/Calendar";
 import Subscribe from "./pages/Subscribe";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-dark text-white">
+    <Router>
+      <div className="flex flex-col min-h-screen bg-dark">
         <Navbar />
         <main className="flex-grow">
           <Routes>
@@ -23,17 +23,25 @@ export default function App() {
             <Route path="/register" element={<Register />} />
             <Route
               path="/dashboard"
-              element={<ProtectedRoute><Dashboard /></ProtectedRoute>}
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
             />
             <Route
               path="/calendar"
-              element={<ProtectedRoute><Calendar /></ProtectedRoute>}
+              element={
+                <ProtectedRoute>
+                  <Calendar />
+                </ProtectedRoute>
+              }
             />
             <Route path="/subscribe" element={<Subscribe />} />
           </Routes>
         </main>
         <Footer />
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
