f1879ef320ad158e53916d356feb4fff80f68da3
dmonster5150-droid <dmonster5150@gmail.com>
2025-11-19 13:05:18 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import import './styles/styles.css';
+import import './styles/styles.css;
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
