a6849f9f46ee8d8c69437f8a5f93d4897823b0db
dmonster5150-droid <dmonster5150@gmail.com>
2026-01-15 19:51:12 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // src/App.jsx
-import { Routes, } from "react-router-dom";
+
 
 import Landing from "./pages/Landing"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
+    
+
 
       <Route path="/provider-dashboard" element={<ProviderDashboard />} />
-    </Routes>
+  
   );
 }
 
