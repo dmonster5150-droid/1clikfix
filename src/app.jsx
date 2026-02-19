1ad9d94afaefa0594babd5683a193054eb8038d9
dmonster5150-droid <dmonster5150@gmail.com>
2026-02-19 12:50:32 -0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -14,3 +14,35 @@ function App() {
 }
 
 export default App;
+import react from "react";
+import { routes, route } from "react-router-dom";
+
+import landing from "./pages/landing";
+import provideronboarding from "./pages/provideronboarding";
+import providerdashboard from "./pages/providerdashboard";
+import payredirect from "./pages/payredirect";
+import admin from "./pages/admin";
+
+function app() {
+
+  return (
+
+    <routes>
+
+      <route path="/" element={<landing />} />
+
+      <route path="/provider-onboarding" element={<provideronboarding />} />
+
+      <route path="/provider-dashboard" element={<providerdashboard />} />
+
+      <route path="/pay" element={<payredirect />} />
+
+      <route path="/admin" element={<admin />} />
+
+    </routes>
+
+  );
+
+}
+
+export default app;
