320da10693d1277ff147e2ea02c71c9247ffbafa
dmonster5150-droid <dmonster5150@gmail.com>
2026-02-20 14:32:30 -0800

    Update pay.jsx

--- src/pages/pages/pay.jsx
+++ src/pages/pages/pay.jsx
@@ -1,1 +1,15 @@
 
+import { use_effect } from "react";
+
+export default function pay(){
+
+  use_effect(()=>{
+
+    window.location.href =
+    "https://square.link/u/your_live_payment_link";
+
+  },[]);
+
+  return null;
+
+}
