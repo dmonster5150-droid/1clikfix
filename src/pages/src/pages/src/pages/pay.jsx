c6bec8d38a3b840d121fb9b14180a8227b2a86e2
dmonster5150-droid <dmonster5150@gmail.com>
2026-02-22 13:27:41 -0800

    Create pay.jsx

---
+++ src/pages/src/pages/src/pages/pay.jsx
@@ -0,0 +1,19 @@
+export default function Pay(){
+
+return(
+
+<div style={{padding:40}}>
+
+<h2>pay now</h2>
+
+<a href="https://square.link/u/YOUR_LINK">
+
+<img src="/qr.png" width="300"/>
+
+</a>
+
+</div>
+
+)
+
+}
