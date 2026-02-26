21ab26ab03397b60fb0dac6b88f9b25ff458343d
dmonster5150-droid <dmonster5150@gmail.com>
2026-02-25 20:19:43 -0800

    Update providercalendar.jsx

--- src/pages/provider/src/pages/providercalendar.jsx
+++ src/pages/provider/src/pages/providercalendar.jsx
@@ -1,3 +1,10 @@
 import { ispai duser } from "../lib/paymentlock"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+const navigate = useNavigate()
+
+useEffect(() => {
+  if (!ispai duser()) {
+    navigate("/providers")
+  }
+}, [])
