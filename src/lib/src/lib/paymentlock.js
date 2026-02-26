e182808352017e665af28cfef67500fc659b032c
dmonster5150-droid <dmonster5150@gmail.com>
2026-02-25 20:14:57 -0800

    Delete src/lib/src/lib directory

--- src/lib/src/lib/paymentlock.js
+++
@@ -1,8 +0,0 @@
-export function ispai duser() {
-  const url = new URL(window.location.href)
-  if (url.searchParams.get("paid") === "true") {
-    localStorage.setItem("provider_paid", "true")
-    return true
-  }
-  return localStorage.getItem("provider_paid") === "true"
-}
