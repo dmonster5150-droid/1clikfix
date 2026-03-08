068efb1945627b141ff8cef9ba6a089542ab3d6e
dmonster5150-droid <dmonster5150@gmail.com>
2026-03-07 23:10:46 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import "./styles/layout.css" React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
+import "./styles/layout.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 <React.StrictMode>
