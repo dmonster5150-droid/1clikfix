2a14677817f8e021a966a7fb7539abd9b88a9349
dmonster5150-droid <dmonster5150@gmail.com>
2026-03-07 23:23:28 -0800

    Update paymentlock.js

--- src/lib/paymentlock.js
+++ src/lib/paymentlock.js
@@ -1,4 +1,4 @@
-export function ispai duser() {
+export function isPaidUser() {
   const url = new URL(window.location.href)
   if (url.searchParams.get("paid") === "true") {
     localStorage.setItem("provider_paid", "true")
