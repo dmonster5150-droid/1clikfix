ffdd8aced9466f300666a020b4289a2b8871704c
dmonster5150-droid <dmonster5150@gmail.com>
2026-03-09 15:15:31 -0700

    Create Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+export default function Logo({ size = 40 }) {
+  return (
+    <div
+      style={{ fontSize: size }}
+      className="font-bold tracking-wide text-red-500"
+    >
+      1ClikFix🧰
+    </div>
+  )
+}
